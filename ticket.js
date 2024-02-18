606c39edb8c7babe947c894a8feef9c2aef1daf6
Munna8383 <jahedmunna612@gmail.com>
2024-02-18 17:56:09 +0600

    Third Push

--- ticket.js
+++ ticket.js
@@ -1,3 +1,165 @@
+
+ const seats  = document.getElementsByClassName("key-button");
+ let seatCount =0
+ let seatLeft  =40;
+ let totalPriceCount=0
+ let clickCount =0
+
+
+
+
+
+
+ for(let seat of seats){
+    seat.addEventListener("click",function(){
+
+        seat.style.backgroundColor = "green"
+        seat.setAttribute('disabled', '');
+    //    increase the number of Seat
+       let seatsAdd= document.getElementById("seat-count");
+
+       let seatText= parseInt(seatsAdd.innerText);
+       seatCount = seatCount+1;
+
+       seatsAdd.innerText = seatCount;
+    //    decrease the number of seat
+       let seatsLeft= document.getElementById("seat-left");
+
+       let seatLeftText= parseInt(seatsAdd.innerText);
+       seatLeft = seatLeft-1;
+
+       seatsLeft.innerText = seatLeft;
+    //    taking all seat reserve by customers
+       let ticketsNumber = document.getElementById("tickets");
+       const div = document.createElement("div")
+       const h1 = document.createElement("h1");
+       const h2 = document.createElement("h1");
+       const h3 = document.createElement("h1");
+
+       div.setAttribute("class","flex");
+       div.classList.add("justify-between");
+       div.classList.add("space-y-3");
+
+       h2.innerText = "Economy";
+
+       h3.innerText = 550;
+
+       let everySeatText = seat.innerText;
+
+       h1.innerText= everySeatText;
+       div.appendChild(h1);
+       div.appendChild(h2);
+       div.appendChild(h3);
+       ticketsNumber.appendChild(div);
+
+    
+
+
+// calculating Total Money
+       totalPriceCount =totalPriceCount+550;
+
+
+       let totalPrice=document.getElementById("total-price");
+       let totalPriceText = parseInt(totalPrice.innerText);
+       totalPrice.innerText=totalPriceCount;
+
+
+    //    calculating Grand Total
+
+    let grandTotal =document.getElementById("grand-total");
+    grandTotal.innerText =totalPriceCount;
+
+
+     clickCount++
+
+     //    restricted to buy more than 4 ticket
+
+    if(clickCount>4){
+        alert("Limit Over");
+        
+    }
+
+    // active the activeButton
+
+    const applyButton = document.getElementById("apply-button")
+
+    if(clickCount>0){
+   
+       applyButton.removeAttribute("disabled")
+   
+    }
+        
+    })
+
+   
+
+
+ }
+
+//  discount Section
+
+ const applyField = document.getElementById("apply-field");
+ const applyButton = document.getElementById("apply-button");
+
+ applyButton.addEventListener("click",function(){
+
+   
+
+    const applyFieldValue = applyField.value;
+    
+    if(applyFieldValue==="NEW15"){
+
+        let discount = totalPriceCount*15/100;
+
+    let fifteenDiscount = totalPriceCount-discount;
+
+        const grandTotal = document.getElementById("grand-total");
+
+        grandTotal.innerText = parseInt(fifteenDiscount);
+
+        const hideApply = document.getElementById("hide-apply");
+
+       hideApply.setAttribute("class","hidden")
+         
+
+        
+    }
+    else if(applyFieldValue==="COUPLE 20"){
+
+        let discount = totalPriceCount*20/100;
+
+        let twentyDiscountDiscount = totalPriceCount-discount;
+
+        const grandTotal = document.getElementById("grand-total");
+
+        grandTotal.innerText =parseInt(twentyDiscountDiscount);
+
+        const hideApply = document.getElementById("hide-apply");
+
+       hideApply.setAttribute("class","hidden")
+
+        
+
+
+    }
+    else{
+        alert("enter Valid Coupon")
+    }
+
+     
+
+
+    
+
+
+ })
+
+
+
+
+
+// traveling another section
+
 function travelToAnotherSection(){
     const travel = document.getElementById("travel")
     travel.scrollIntoView({ behavior: 'smooth' });
